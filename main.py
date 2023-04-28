9f03bccc5290d980ae012ba807d3c2223a73842a
Silas <s9956385@stud.uni-frankfurt.de>
2023-04-28 18:02:25 -0400

    all models

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import json
 
 from llama_cpp import Llama
 
-print('Loading model')
-llm = Llama(model_path='/home/stud_homes/s9956385/models/ggml-alpaca-7b-q4.bin')
-print('Model loaded')
-output = llm(
-    'Question: Name 10 words that are as different from each other as possible? Answer:',
-    max_tokens = 100,
-    temperature=0.8,
-    stop= ['\n', 'Question:', 'Q:'],
-    echo = True,
-)
+for i in ['ggml-alpaca-7b-q4.bin', 'ggml-model-q4_0.bin', 'ggml-vicuna-7b-1.1-q4_0.bin',  'gpt4all-lora-quantized.bin']:
+    print('Loading model' + i)
+    llm = Llama(model_path='/home/stud_homes/s9956385/models/' + i)
+    print('Model loaded')
+    output = llm(
+        'Question: Name 10 words that are as different from each other as possible? Answer:',
+        max_tokens = 100,
+        temperature=0.8,
+        stop= ['\n', 'Question:', 'Q:'],
+        echo = True,
+    )
 
-print(output)
+    print(output)
